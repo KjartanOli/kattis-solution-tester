8102f854f540395f476eb59a5247da417438b9e5
Kjartan Óli Ágústsson <kjartanoli@protonmail.com>
2020-01-24 21:25:36 +0000

    Renamed LICENSE to COPYING in accordance to GNU conventions

--- kst.py
+++ kst.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
 
+# kst (Kattis Solution Tester), compare the output of SOLUTION to the expected
+# output of the specified problem on kattis.com
+
+# Copyright (C) 2020  Ágústsson, Kjartan Óli
+# Author: Ágústsson, Kjartan Óli <kjartanoli@protonmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys, requests, subprocess, zipfile, io, os
 from enum import Enum, auto
 
@@ -27,6 +46,7 @@ class Cache:
 	def contains(this, subdomain, problem):
 		"""Check if the cache contains the specified problem."""
 		dir = subdomain + '/' + problem + '/'
+		# if the cache doesn't exist the problem can't be in it
 		if this.file not in os.listdir():
 			return False
 		with zipfile.ZipFile(this.file, 'r') as cache:
@@ -39,6 +59,13 @@ class Cache:
 		with zipfile.ZipFile(this.file, 'a') as cache:
 			cache.writestr(fileName, content)
 
+	def clear(this):
+		"""Clear the cache."""
+		# if the cache exists
+		if this.file in os.listdir():
+			# remove it
+			os.remove(this.file)
+
 	def load(this, subdomain, problem):
 		"""Load a problem from the cache."""
 		samples = []
@@ -49,6 +76,7 @@ class Cache:
 				if not os.path.isdir(name):
 					with cache.open(name) as file:
 						if ".in" in name:
+							# store the content of the file to append to the list of samples along with their output
 							tmp = file.read().decode("utf-8").strip()
 						else:
 							samples.append(Sample(tmp, file.read().decode("utf-8").strip()))
@@ -61,25 +89,167 @@ class FileType(Enum):
 	JAVA = auto()
 	JS = auto()
 
+def main(argc, argv):
+	global python2, python3, verbose, compiler, noCache
+	# Should you skip the current argument
+	skip = False
+	for i, arg in enumerate(argv):
+		if skip:
+			skip = False
+			continue
+		else:
+			if arg == "-ice" or arg == "--iceland":
+				subdomain = "iceland"
+			elif arg == "--open":
+				subdomain = "open"
+			elif arg == "-s" or arg == "--subdomain":
+				subdomain = argv[i + 1]
+				# the next argument is a part of the current flag
+				skip = True
+			elif arg == "-p2" or arg == "--python2":
+				if python3:
+					incompatable_python()
+					return 1
+				else:
+					python2 = True
+			elif arg == "-p3" or arg == "--python3":
+				if python2:
+					incompatable_python()
+					return 1
+				else:
+					python3 = True
+			elif arg == "-c" or arg == "--compiler":
+				compiler = argv[i + 1]
+				# the next argument is a part of the current flag
+				skip = True
+			elif arg == "-v" or arg == "--verbose":
+				verbose = True
+			elif arg == "-V" or arg == "--version":
+				# print version inforation and exit
+				version()
+				return 0
+			elif arg == "-h" or arg == "--help":
+				# print help and exit
+				help()
+				return 0
+			elif arg == "-nc" or arg == "--no-cache":
+				# don't add the problem to the cache
+				noCache = True
+			elif arg == "-cc" or arg == "--clear-cache":
+				# clear the cache and exit
+				cache.clear()
+				return 0
+			elif arg == "-co" or arg  == "--cache-only":
+				cacheOnly = True
+			# the second last argument should be the id of the problem
+			elif i == argc - 2:
+				problem = arg
+			# the last argument should bo the path to the users solution
+			elif i == argc - 1:
+				solution = arg
+
+	if not cache.contains(subdomain, problem):
+		if not noCache:
+			samples = download_samples(subdomain, problem)
+		else:
+			sys.stderr.write("Problem samples not in cache\n")
+			return 3
+
+	else:
+		print(cacheOnly, "test")
+		if not cacheOnly:
+			print("Test")
+			samples = cache.load(subdomain, problem)
+
+	fileType = get_fileType(solution, True if verbose else False)
+
+	if is_compiled(fileType):
+		try:
+			compile(solution)
+			if fileType is FileType.JAVA:
+				interpreter = "java"
+				solution = solution[:-5]
+			else:
+				interpreter = "./a.out"
+		except CompileErrorExceptinon as e:
+			if verbose:
+				sys.stderr.write(str(e) + '\n')
+			else:
+				sys.stderr.write("Compile Error\n")
+			return 2
+	else:
+		interpreter = set_interpreter(fileType)
+
+	l = len(samples) - 1
+	for i, sample in enumerate(samples):
+		try:
+			res = "Passed" if test(i, sample, solution, interpreter) else "Failed"
+			sys.stdout.write("Test " + str(i + 1) + ": " + res + ("\n\n" if i < l and verbose else '\n'))
+		except RunntimeErrorException as e:
+			if verbose:
+				sys.stderr.write(str(e) + '\n')
+			else:
+				sys.stderr.write("Test " + str(i + 1) + ": Runntime Error\n")
+
+	if is_compiled(fileType):
+		if fileType is FileType.JAVA:
+			# javac creates a file with the same name as the java file and the .class extention
+			clean_up(solution[:-5] + ".class")
+		else:
+			# the default output of gcc and g++ is a.out
+			clean_up("a.out")
+
+	return 0
 def version():
 	"""Print version information."""
-	sys.stdout.write("kst (Kattis Solution Tester) 2.0.0\n\nWritten by Kjartan Óli Ágústsson\n")
+	sys.stdout.write("kst (Kattis Solution Tester) 2.0.1 Copyright (C) " + year +
+		' ' + author + "\nThis program comes with ABSOLUTELY NO WARRANTY. This is " +
+		"free software,\nand you are welcome to redistribute it")
 
 def help():
 	"""Print help."""
-	sys.stdout.write("Usage: kst [OPTIONS] PROBLEM_ID SOLUTION\nCompare the output of SOLUTION to the sample outputs provided for the problem\n")
+	# indent for options listing
+	indent = "  "
+	sys.stdout.write("Usage: kst [OPTIONS] PROBLEM SOLUTION\nCompare the output" +
+	" of SOLUTION to the sample outputs provided for the problem\nExample: kst -ice" +
+	"budarkassi1 budarkassi1.py\nDomain control:\n" + indent + "-ice, --iceland\t\t" +
+	"Set the subdomain to iceland\n" + indent + "\t--open\t\t\tSet the subdomain to" +
+	" open\n" + indent + "-s,\t--subdomain=SUBDOMAIN\t Set subdomain to SUBDOMAIN\n\n" +
+	"(Setting the subdomain to 'iceland' automaticly adds the 'iceland.' in" +
+	" all iceland problems to the problem's id)\n\n Testing control:\n" + indent + "-p2," +
+	"\t--python2\t\tSpecify that the solution should be tested with python2\n" + indent +
+	"-p3,\t--python3\t\tSpecify that the solution should be tested with python3\n" +
+	indent + "-c,\t--compiler=COMPILER\t Specify that the solution should be compiled" +
+	"using COMPILER\n\n Cache control:\n" + indent + "-nc,\t--no-cache\t\tDon't add the" +
+	"samples for this problem to the cache\n" + indent + "-cc,\t--clear-cache \t\tClear" +
+	" the cache and exit\n" + indent + "-co,\t--cache-only\t\tOnly use samples from" +
+	" the cache\n\nError codes:\n" + indent + "1\t\tIncompatable options --python2" +
+	" and --python3\n\nkst (Kattis Solution Tester) Copyright (C) " + year + ' ' + author +
+	"\nThis program comes with ABSOLUTELY NO  WARRANTY. This is free software,\nand" +
+	" you are welcome to redistribute it\n")
 
-def download_samples(url):
+def get_url(subdomain, problem):
+	"""Get the url for the problem"""
+	if subdomain == "iceland":
+		return "https://" + subdomain + ".kattis.com/problems/iceland." + problem + "/file/statement/samples.zip"
+	else:
+		return "https://" +  subdomain + ".kattis.com/problems/" + problem + "/file/statement/samples.zip"
+
+
+
+def download_samples(subdomain, problem):
 	"""Download samples for the current problem."""
-	files = requests.get(url)
+	files = requests.get(get_url(subdomain, problem))
 	samples = []
 	tmp = ""
 	with zipfile.ZipFile(io.BytesIO(files.content)) as zip:
 		for zipinfo in zip.infolist():
 			with zip.open(zipinfo) as file:
 				content = file.read().decode("utf-8").strip()
-				cache.add(subdomain + '/' + problem + '/' + file.name, content)
+				if not noCache:
+					cache.add(subdomain + '/' + problem + '/' + file.name, content)
 				if ".in" in file.name:
+					# store the content of the file to append to the list of samples along with their output
 					tmp = content
 				else:
 					samples.append(Sample(tmp, content))
@@ -94,7 +264,7 @@ def test(testNum, sample, solution, interpreter):
 	out, err = proc.communicate(input=sample.input.encode())
 
 	out = out.decode("utf-8").strip()
-	err = err.decode("utf-8")
+	err = err.decode("utf-8").strip()
 
 	if err != "":
 		raise RunntimeErrorException(err)
@@ -102,6 +272,7 @@ def test(testNum, sample, solution, interpreter):
 		if verbose:
 			sys.stdout.write(out + "\nExpected output:\n" + sample.output + "\n")
 
+		# compare the output to the expected output
 		if out == sample.output:
 			return True
 		else:
@@ -148,10 +319,10 @@ def set_interpreter(fileType):
 	if verbose:
 		sys.stdout.write("Setting interpreter to: ")
 		if fileType is FileType.PYTHON:
-			if p2:
+			if python2:
 				sys.stdout.write("python2\n\n")
 				return "python2"
-			if p3:
+			if python3:
 				sys.stdout.write("python3\n\n")
 				return "python3"
 			sys.stdout.write("python\n\n")
@@ -163,9 +334,9 @@ def set_interpreter(fileType):
 
 	else:
 		if fileType is FileType.PYTHON:
-			if p2:
+			if python2:
 				return "python2"
-			if p3:
+			if python3:
 				return "python3"
 			return "python"
 		if fileType is FileType.JS:
@@ -211,96 +382,20 @@ def incompatable_python():
 	"""Print an error indicating incompatable options."""
 	sys.stderr.write("Incompatable options --python2 and --python3\n")
 
-def main(argc, argv):
-	global p2, p3, verbose, compiler, problem, subdomain
-	skip = False
-	for i, arg in enumerate(argv):
-		if skip:
-			skip = False
-			continue
-		else:
-			if arg == "-ice" or arg == "--iceland":
-				subdomain = "iceland"
-			elif arg == "--open":
-				subdomain = "open"
-			elif arg == "-s" or arg == "--subdomain":
-				subdomain = argv[i + 1]
-				skip = True
-			elif arg == "-p2" or arg == "--python2":
-				if p3:
-					incompatable_python()
-					return 1
-				p2 = True
-			elif arg == "-p3" or arg == "--python3":
-				if p2:
-					incompatable_python()
-					return 1
-				p3 = True
-			elif arg == "-c" or arg == "--compiler":
-				compiler = argv[i + 1]
-				skip = True
-			elif arg == "-v" or arg == "--verbose":
-				verbose = True
-			elif arg == "-V" or arg == "--version":
-				version()
-				return 0
-			elif arg == "-h" or arg == "--help":
-				help()
-				return 0
-			elif i == argc - 2:
-				problem = arg
-			elif i == argc - 1:
-				solution = arg
+def clean_up(file):
+	"""Cleon up executables created by compiling."""
+	os.remove(file)
 
-	if subdomain == "iceland":
-		url = "https://" + subdomain + ".kattis.com/problems/iceland." + problem + "/file/statement/samples.zip"
-	else:
-		url = "https://" +  subdomain + ".kattis.com/problems/" + problem + "/file/statement/samples.zip"
 
-	if not cache.contains(subdomain, problem):
-		samples = download_samples(url)
-	else:
-		samples = cache.load(subdomain, problem)
-
-	fileType = get_fileType(solution, True if verbose else False)
-
-	if is_compiled(fileType):
-		try:
-			compile(solution)
-			if fileType is FileType.JAVA:
-				interpreter = "java"
-				solution = solution[:-5]
-			else:
-				interpreter = "./a.out"
-		except CompileErrorExceptinon as e:
-			if verbose:
-				sys.stderr.write(str(e) + '\n')
-			else:
-				sys.stderr.write("Compile Error\n")
-			return 2
-	else:
-		interpreter = set_interpreter(fileType)
-
-	l = len(samples) - 1
-	for i, sample in enumerate(samples):
-		try:
-			res = "Passed" if test(i, sample, solution, interpreter) else "Failed"
-			sys.stdout.write("Test " + str(i + 1) + ": " + res + ("\n\n" if i < l and verbose else '\n'))
-		except RunntimeErrorException as e:
-			if verbose:
-				sys.stderr.write(str(e) + '\n')
-			else:
-				sys.stderr.write("Test " + str(i + 1) + ": Runntime Error\n")
-			#sys.stdout.write("Excpected output:\n" + sample.output + '\n')
-
-	return 0
-
-p2 = False
-p3 = False
+python2 = False
+python3 = False
 verbose = False
+noCache = False
 compiler = ""
-problem = ""
-subdomain = ""
+
+year = "2020"
+author = "Kjartan Óli Ágústsson"
+
 cache = Cache(".cache")
 
 sys.exit(main(len(sys.argv), sys.argv))
